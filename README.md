1bfe5d276f9b37c5f56315fecbc76735c8a80ad7
Serge Fomin <serge.fo@gmail.com>
2017-03-20 23:57:15 -0700

    Implemented import sub command: extracting data from the spreadsheet, parsing the spec and interacting with selected API resource. Using older swagger-client version 2.1.32 for now.

--- README.md
+++ README.md
@@ -3,18 +3,44 @@
 ```
 Usage: openapi-workbook [options] [command]
 
- Commands:
 
-   template <spec> <filename>  Generate an empty "template" workbook from OpenAPI 2.0 spec
-   export <spec> <filename>    Export data from workbook to API resource(s)
-   import <spec> <filename>    Import data from API resource(s) to a workbook
+  Commands:
 
- Options:
+    template <spec> <filename>          Generate an empty "template" workbook from OpenAPI 2.0 spec
+    export <spec> <filename>            Export data from workbook to API resource(s)
+    import [options] <spec> <filename>  Import data from API resource(s) to a workbook
 
-   -h, --help     output usage information
-   -V, --version  output the version number
+  Options:
+
+    -h, --help     output usage information
+    -V, --version  output the version number
 ```
 
+## Template
+
+Example:
+
 ```
-#./bin/openapi-workbook.js template http://petstore.swagger.io/v2/swagger.json tmp/out.xlsx
+./bin/openapi-workbook.js template http://petstore.swagger.io/v2/swagger.json tmp/out.xlsx
+```
+
+## Import
+
+```
+  Usage: import [options] <spec> <filename>
+
+  Import data from API resource(s) to a workbook
+
+  Options:
+
+    -h, --help                       output usage information
+    -r, --resource <resource>        path to the resource
+    -p, --parameter <parameter>      name of the parameter to use, defaults to "body"
+    -s, --spreadsheet <spreadsheet>  name of the spreadsheet to import data from
+```
+
+Example:
+
+```
+./bin/openapi-workbook.js import --resource /pet --spreadsheet Pet http://petstore.swagger.io/v2/swagger.json tmp/out.xlsx
 ```
--- bin/openapi-workbook.js
+++ bin/openapi-workbook.js
@@ -1,44 +1,39 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 
-var program = require("commander"),
-  chalk = require("chalk"),
-  tool = require("../");
+const program = require('commander');
+const chalk = require('chalk');
+const tool = require('../');
 
 program
-  .command("template <spec> <filename>")
+  .command('template <spec> <filename>')
   .description('Generate an empty "template" workbook from OpenAPI 2.0 spec')
-  .action(function(spec, filename) {
+  .action((spec, filename) => {
     tool.template(spec, filename);
   });
 
 program
-  .command("export <spec> <filename>")
+  .command('export <spec> <filename>')
   .description('Export data from workbook to API resource(s)')
-  .action(function(spec, filename) {
+  .action((spec, filename) => {
     tool.export(spec, filename);
   });
 
 program
-  .command("import <spec> <filename>")
+  .command('import <spec> <filename>')
   .description('Import data from API resource(s) to a workbook')
-  .action(function(spec, filename) {
-    tool.import(spec, filename);
+  .option('-r, --resource <resource>', 'path to the resource')
+  .option('-p, --parameter <parameter>', 'name of the parameter to use, defaults to "body"')
+  .option('-s, --spreadsheet <spreadsheet>', 'name of the spreadsheet to import data from')
+  .action((spec, filename, options) => {
+    tool.import(spec, filename, options);
   });
 
 program
-  .version(require("../package").version)
-  .on("debug", function(filter) {
-    process.env.DEBUG = filter ||
-      "openapi-workbook:*,swagger:*,json-schema-ref-parser";
+  .version(require('../package').version)
+  .on('debug', (filter) => {
+    process.env.DEBUG = filter || 'openapi-workbook:*,swagger:*,json-schema-ref-parser';
   })
   .parse(process.argv);
 
-if (program.rawArgs.length < 3) {
-  program.help();
-}
-
-function errorHandler(err) {
-  console.error(chalk.red(err.stack));
-  process.exit(1);
-}
+tool.cli.validate(program);
